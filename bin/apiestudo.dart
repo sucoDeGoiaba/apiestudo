641525be40494344e90007dcb8575b03e9a8d974
sucoDeGoiaba <flp.coding@gmail.com>
2023-12-12 15:24:55 -0300

    iniciando api

--- bin/apiestudo.dart
+++ bin/apiestudo.dart
@@ -1,5 +1,10 @@
-import 'package:apiestudo/apiestudo.dart' as apiestudo;
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
 
-void main(List<String> arguments) {
-  print('Hello world: ${apiestudo.calculate()}!');
+// Main - Entry point, onde a aplicação começa a ser executada;
+void main() async {
+  final server = await shelf_io.serve(
+      (request) => Response(200, body: 'Olá mundo!'), 'localhost', 8080);
+
+  print("Servidor iniciado: http://localhost:8080");
 }
